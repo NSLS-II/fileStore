77f46cec9a541c84290449a4221edbaacf384dec
Timothy Liu <chbrahmsliu@gmail.com>
2015-11-18 21:27:14 -0500

    make-up change
    make point_number an integer
    follow PEP8 convention
    
    Hope that look better

--- filestore/handlers.py
+++ filestore/handlers.py
@@ -60,15 +60,18 @@ class AreaDetectorTiffHandler(HandlerBase):
         self._image_sequence = pims.ImageSequence(self._filename)
 
     def __call__(self, point_number):
-        start= int(point_number * self._fpp)
-        stop = int((point_number + 1) * self._fpp)
+        point_number = int(point_number)
+        start= point_number * self._fpp
+        stop = (point_number + 1) * self._fpp
         if stop > len(self._image_sequence):
             # if asking for an image past the end, make sure we have an up to
             # date list of the existing files
             self._image_sequence = pims.ImageSequence(self._filename)
             if stop > len(self._image_sequence):
                 # if we _still_ don't have enough files, raise
-                raise IntegrityError("Seeking Frame {0} out of {1} frames.".format(stop, len(self._image_sequence)))
+                raise IntegrityError(
+                          "Seeking Frame {0} out of {1} frames.".format(
+                              stop, len(self._image_sequence)))
         return np.asarray(list(self._image_sequence[start:stop])).squeeze()
 
 
